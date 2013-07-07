3ba6705fdda44959fdc2c24a11cf21be7a9a7777
heileng <heileng219@gmail.com>
2013-07-07 12:59:26 +0800

    finish cover+img normalization,finish avatar_bar shader

--- Breeze/src/com/breeze/movieflow/fragments/HubFragment.java
+++ Breeze/src/com/breeze/movieflow/fragments/HubFragment.java
@@ -5,6 +5,7 @@ import com.breeze.movieflow.utils.BitmapHelper;
 
 import android.app.Activity;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -15,6 +16,7 @@ import android.widget.ImageView;
 
 public class HubFragment extends Fragment {
 	ImageView avatar;
+	ImageView cover;
 
 	public HubFragment() {
 		// TODO Auto-generated constructor stub
@@ -27,12 +29,23 @@ public class HubFragment extends Fragment {
 		View layout = inflater.inflate(R.layout.hub_fragment_layout, container,
 				false);
 		avatar = (ImageView) layout.findViewById(R.id.avatar);
+		//处理封面
+		cover = (ImageView) layout.findViewById(R.id.cover);
+		BitmapHelper imagehelper = new BitmapHelper();
+		//获取封面图片
+		Drawable cover_img=cover.getDrawable();
+		//规格化图片
+		Bitmap cover_normalized_img=imagehelper.normalizeCoverImage(cover_img);
+		//为规格化后的图片封面创建阴影
+		Bitmap cover_normalized_shadow_img =imagehelper.createBitmapShadow(cover_normalized_img);
+		cover.setImageBitmap(cover_normalized_shadow_img);
 
+		//处理头像
 		Resources res = getResources();
 		Drawable image = res.getDrawable(R.drawable.avatar);
 		Activity activity=getActivity();
 
-		BitmapHelper imagehelper = new BitmapHelper();
+		
 		avatar.setImageBitmap(imagehelper.createAvatarBar(imagehelper.makeRectImage(image),activity));
 		return layout;
 	}
--- Breeze/src/com/breeze/movieflow/utils/BitmapHelper.java
+++ Breeze/src/com/breeze/movieflow/utils/BitmapHelper.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
@@ -12,17 +13,68 @@ import android.graphics.PorterDuff.Mode;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Shader;
 import android.graphics.Typeface;
 import android.graphics.Xfermode;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 
 @SuppressWarnings("deprecation")
 public class BitmapHelper {
 	private static final Xfermode MASK_XFERMODE;
 	private int shadowwidth = 30;
+	private static float rate = 1.325f;
 	static {
 		PorterDuff.Mode localMode = PorterDuff.Mode.DST_IN;
 		MASK_XFERMODE = new PorterDuffXfermode(localMode);
+	}
+
+	public Bitmap normalizeCoverImage(Drawable cover) {
+		Bitmap bmp = drawableToBitmap(cover);
+		float bmp_w = bmp.getWidth();
+		float bmp_h = bmp.getHeight();
+		Log.e("输出信息", "" + bmp_w);
+		Log.e("输出信息", "" + bmp_h);
+		Bitmap output = null;
+		final Paint paint = new Paint();
+		paint.setAntiAlias(true);
+		float bmp_rate = bmp_w / bmp_h;
+		Log.e("输出信息", "" + bmp_rate);
+		float canvas_w, canvas_h;
+		// bmp_rate大于设定rate则选择高度填充
+		// bmp_rate小于设定rate则选择宽度填充
+		if (bmp_rate > rate) {
+			Log.e("输出信息", "开始执行>操作");
+			canvas_h = bmp_h;
+			canvas_w = rate * canvas_h;
+			float a = (bmp_w - canvas_w) / 2;
+			float b = a + canvas_w;
+			// 创建画布
+			output = Bitmap.createBitmap((int) canvas_w, (int) canvas_h,
+					Config.ARGB_8888);
+			Canvas canvas = new Canvas(output);
+			// 设置原图截取范围
+			final Rect rect = new Rect((int) a, 0, (int) b, (int) canvas_h);
+			// 设置绘制范围
+			final RectF rectF = new RectF(0, 0, canvas_w, canvas_h);
+			canvas.drawBitmap(bmp, rect, rectF, paint);
+		} else if (bmp_rate <= rate) {
+			Log.e("输出信息", "开始执行<操作");
+			canvas_w = bmp_w;
+			canvas_h = canvas_w / rate;
+			float a = (bmp_h - canvas_h) / 2;
+			float b = a + canvas_h;
+			// 创建画布
+			output = Bitmap.createBitmap((int) canvas_w, (int) canvas_h,
+					Config.ARGB_8888);
+			Canvas canvas = new Canvas(output);
+			// 设置原图截取范围
+			final Rect rect = new Rect(0, (int) a, (int) canvas_w, (int) b);
+			// 设置绘制范围
+			final RectF rectF = new RectF(0, 0, canvas_w, canvas_h);
+			canvas.drawBitmap(bmp, rect, rectF, paint);
+		}
+		return output;
 	}
 
 	// 创建圆角矩形头像
@@ -115,17 +167,25 @@ public class BitmapHelper {
 	}
 
 	// 为图片添加阴影效果
-	public Bitmap createBitmapShadow(Drawable image) {
-		Bitmap bmp = drawableToBitmap(image);
-		Bitmap output = Bitmap.createBitmap(bmp.getWidth() + shadowwidth,
-				bmp.getHeight() + shadowwidth, Config.ARGB_8888);
+	public Bitmap createBitmapShadow(Bitmap bmp) {
+		// Bitmap bmp = drawableToBitmap(image);
+		Bitmap output = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight()
+				+ shadowwidth, Config.ARGB_8888);
 		Canvas canvas = new Canvas(output);
 		final Paint paint = new Paint();
 		paint.setAntiAlias(true);
-		paint.setShadowLayer(15, -5, 0, Color.argb(200, 10, 10, 10));
+		// 绘制阴影层
+		paint.setShadowLayer(10, -5, 0, Color.argb(200, 10, 10, 10));
+		final Rect rectbc = new Rect(0, 0, bmp.getWidth(), bmp.getHeight());
+		final RectF rectbcF = new RectF(rectbc);
+		paint.setColor(Color.argb(100, 50, 50, 50));
+		canvas.drawRect(rectbcF, paint);
+		// 重新绘制图片
+		paint.setShadowLayer(0, -5, 0, Color.argb(200, 10, 10, 10));
+		paint.setColor(Color.argb(255, 50, 50, 50));
 		final Rect rect_bmp = new Rect(0, 0, bmp.getWidth(), bmp.getHeight());
-		final Rect rect_can = new Rect(shadowwidth, 0, bmp.getWidth()
-				+ shadowwidth, bmp.getHeight());
+		final Rect rect_can = new Rect(0, 0, bmp.getWidth() + shadowwidth,
+				bmp.getHeight());
 		canvas.drawBitmap(bmp, rect_bmp, rect_can, paint);
 
 		return output;
@@ -143,8 +203,15 @@ public class BitmapHelper {
 		final Rect rectbc = new Rect(shadowwidth, 0, 4 * avatar.getWidth()
 				+ shadowwidth, avatar.getHeight());
 		final RectF rectbcF = new RectF(rectbc);
-		paint.setShadowLayer(15, -5, 0, Color.argb(200, 10, 10, 10));
+		paint.setShadowLayer(10, -5, 5, Color.argb(200, 10, 10, 10));
 		paint.setColor(Color.argb(100, 50, 50, 50));
+		canvas.drawRect(rectbcF, paint);
+		// 画渐变
+		Shader mShader = new LinearGradient(0, 0, 4*avatar.getWidth(),
+				avatar.getHeight(), new int[] { Color.argb(35, 255, 255, 255),
+						Color.argb(0, 255, 255, 255) }, null,
+				Shader.TileMode.REPEAT);
+		paint.setShader(mShader);
 		canvas.drawRect(rectbcF, paint);
 
 		paint.setColor(Color.argb(255, 0, 0, 0));
@@ -157,15 +224,16 @@ public class BitmapHelper {
 		final Rect rect_avatar = new Rect(0, 0, avatar.getWidth(),
 				avatar.getHeight());
 		// canvas.drawRect(rectF, paint);
+		paint.setShader(null);
 		paint.setXfermode(new PorterDuffXfermode(Mode.SRC_OVER));
 		canvas.drawBitmap(avatar, rect_avatar, rect_draw_F, paint);
-		paint.setColor(Color.WHITE);
+		paint.setColor(Color.argb(255, 255, 255, 255));
 		Typeface font = Typeface.createFromAsset(context.getAssets(),
-				"fonts/berlin.ttf");
+				"fonts/Capture it.ttf");
 		paint.setTypeface(font);
 		paint.setTextSize(70);
 		canvas.drawText("HEILENG", avatar.getWidth() + 30 + shadowwidth,
-				avatar.getHeight() / 2 + 70, paint);
+				avatar.getHeight() / 2 + 50, paint);
 		return output;
 	}
 
